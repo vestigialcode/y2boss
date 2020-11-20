00f36844d24617d1c01cef51eb91a88695c4ec86
vestigialcode <dibyajiban232@gmail.com>
2020-11-20 14:30:07 +0530

    added basic server logic

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("Server started... ");
+const express= require('express');
+const morgan = require('morgan');
+const cors= require('cors');
+
+const app= express();
+app.listen('5050',()=>console.log("Server started at port 5050"));
+
+
 
