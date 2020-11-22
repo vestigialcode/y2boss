80939d2bd9d6e92825ec4bb68ed857ac5aeb85d2
vestigialcode <dibyajiban232@gmail.com>
2020-11-22 17:02:24 +0530

    added index file and routing logic

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express= require('express');
-const morgan = require('morgan');
-const cors= require('cors');
-
-const app= express();
-app.listen('5050',()=>console.log("Server started at port 5050"));
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+//Redirecting to static directory
+app.use('/static', express.static('./static'));
+app.get('/', (req, res) => { 
+    res.sendFile('index.html', { root: './' });
+});
+//Adding api endpoint
+app.get('/download', (req, res) => {
+    var url = req.query.url;    
+    res.header("Content-Disposition", 'attachment;\  filename="Video.mp4');    
+    ytdl(url, {format: 'mp4'}).pipe(res);
+});
+app.listen(3000, () => { 
+    console.log(`Server started...`);
+});
 
 
 
